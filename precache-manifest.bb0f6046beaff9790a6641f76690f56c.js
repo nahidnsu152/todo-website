d5150fe5ebde27da598dd84df8d5580688aa4701
Nazmul Hasan <nahid.nsu152@gmail.com>
2020-09-19 21:46:04 +0600

    Updates

---
+++ precache-manifest.bb0f6046beaff9790a6641f76690f56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdcc15adfbcd32897b5b65a8cc9343d",
+    "url": "/nahidnsu152.github.io/todo-website/index.html"
+  },
+  {
+    "revision": "92eda8fc863ae814081c",
+    "url": "/nahidnsu152.github.io/todo-website/static/css/main.d64278ca.chunk.css"
+  },
+  {
+    "revision": "23b577c293a1b755b605",
+    "url": "/nahidnsu152.github.io/todo-website/static/js/2.a15baf37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nahidnsu152.github.io/todo-website/static/js/2.a15baf37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92eda8fc863ae814081c",
+    "url": "/nahidnsu152.github.io/todo-website/static/js/main.dc28a542.chunk.js"
+  },
+  {
+    "revision": "ee0a35b41a0533b44f12",
+    "url": "/nahidnsu152.github.io/todo-website/static/js/runtime-main.da7c781a.js"
+  }
+]);
